1b6a5557e028fb3782256fec6d99307cacec0f14
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2022-01-22 16:46:09 +0100

    Add extension for accessing data in post

---
+++ Blog/Helpers/DocumentExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Statiq.Common;
+
+namespace Blog.Helpers;
+
+public class BlogPost
+{
+    private readonly IDocument? _document;
+    public BlogPost(IDocument? document)
+    {
+        _document = document;
+    }
+
+    public string? Title => _document.GetString(Keys.Title);
+    public string? Permalink => _document.GetString("permalink");
+    public string? Excerpt => _document.GetString(Keys.Excerpt);
+    public string? Date => _document.GetDateTime("date").ToString("MMM dd, yyyy"); //TODO extract date format in configuration from en.yml
+    public string? ThumbnailImage => _document.GetString("thumbnailImage");
+}
+
+public static class DocumentExtensions
+{
+    private static BlogPost AsBlogPost(this IDocument? document)
+    {
+        return new BlogPost(document);
+    }
+
+    public static IEnumerable<BlogPost> AsBlogPost(this DocumentList<IDocument> blogs)
+    {
+        return blogs.Select(blog => blog.AsBlogPost());
+    }
+}
