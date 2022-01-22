a303d8e34a2d6e5e85cdf774a76f62b0a64d18e2
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2022-01-22 14:28:42 +0100

    SAVEPOINT

--- Blog/Helpers/TranslationExtensions.cs
+++ Blog/Helpers/TranslationExtensions.cs
@@ -8,10 +8,20 @@ namespace Blog.Helpers;
 /// </summary>
 public static class TranslationExtensions
 {
-    public static string Translate(this StatiqRazorPage<IDocument> page, string category, string key)
+    public static string Translate(this StatiqRazorPage<IDocument> page, string translationCategory, string key)
     {
-        var translation = page.Outputs.FromPipeline("Data").FilterSources($"languages/{Constants.Language}.yml").First();
+        return GetTranslation(page).GetMetadata(translationCategory).GetString(key);
+    }
 
-        return translation.GetMetadata(category).GetString(key);
+    public static string TranslateFormatted(this StatiqRazorPage<IDocument> page, string translationCategory, string formattedKey, string value)
+    {
+        var format = GetTranslation(page).GetMetadata(translationCategory).GetString(formattedKey);
+        
+        return string.Format(format, value);
+    }
+
+    private static IDocument GetTranslation(StatiqRazorPage<IDocument> page)
+    {
+        return page.Outputs.FromPipeline("Data").FilterSources($"languages/{Constants.Language}.yml").First();
     }
 }
