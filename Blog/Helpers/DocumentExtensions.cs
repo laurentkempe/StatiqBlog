b884fd5ebfae959e0672df184d1e044c50b4f6fa
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2022-01-22 17:01:48 +0100

    Get date format from config

--- Blog/Helpers/DocumentExtensions.cs
+++ Blog/Helpers/DocumentExtensions.cs
@@ -13,7 +13,7 @@ public class BlogPost
     public string? Title => _document.GetString(Keys.Title);
     public string? Permalink => _document.GetString("permalink");
     public string? Excerpt => _document.GetString(Keys.Excerpt);
-    public string? Date => _document.GetDateTime("date").ToString(_document.GetDateFormat());
+    public string? Date => _document.GetDateTime("date").ToString(_document?.GetDateFormat());
     public string? ThumbnailImage => _document.GetString("thumbnailImage");
 }
 
