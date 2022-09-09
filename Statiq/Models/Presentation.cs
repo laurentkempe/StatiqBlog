793b61a2524279c7e302cbf0fa026f274164ddab
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2022-09-09 18:06:10 +0200

    Show tags for presentation and list presentation tags and display it on all-tags

--- Statiq/Models/Presentation.cs
+++ Statiq/Models/Presentation.cs
@@ -7,6 +7,8 @@ public class Presentation : BlogElementBase
     public Presentation(IDocument document) : base(document)
     {
     }
+    
+    public IEnumerable<string> Tags => Document.GetList<string>("tags") ?? Enumerable.Empty<string>();
 
     public string Video => Document.GetString("video");
     public string VideoStart => Document.GetString("start") ?? "0";
